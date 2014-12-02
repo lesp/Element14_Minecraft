a05d876b6f4db10d1e50516ecf15ed8931aadb62
Les <lespounder@gmail.com>
2014-12-02 07:30:55 +0000

    Update and rename MC_Teleport_button.py to MC_Project_2.py

--- MC_Project_2.py
+++ MC_Project_2.py
@@ -5,7 +5,6 @@ from random import randint
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(27, GPIO.OUT)
 
 mc = minecraft.Minecraft.create()
 while True:
@@ -14,9 +13,7 @@ while True:
         b = (randint(0,100))
         c = (randint(0,100))
         GPIO.wait_for_edge(17, GPIO.FALLING)
-        GPIO.output(27, True)
         pos = mc.player.getPos()
         mc.player.setPos(a,b,c)
         mc.postToChat("ENERGIZE!!!")
         sleep(5)
-        GPIO.output(27, False)
